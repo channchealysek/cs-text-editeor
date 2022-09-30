23c1467ce636e4f9d62cb2138413b76d9b233717
Channchealy Sek <sek.channchealy@yahoo.com>
2022-09-30 17:03:08 -0500

    build production

---
+++ client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+  // PUT function
+export const putDb = async (content) => {
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ content })
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
+
+// GET function
+export const getDb = async () => {
+  console.log('Getting data from the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.getAll()
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
+
+initdb();
